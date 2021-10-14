843f80115352507dd70954592a2fe378ad73ed7f
hyperbotauthor <73212952+hyperbotauthor@users.noreply.github.com>
2021-10-14 23:13:01 +0000

    Octokit login

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,6 +1,12 @@
+const { Octokit } = require("octokit")
+
+const octokit = new Octokit({ auth: process.env["GIT_TOKEN"] })
+
 exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: "netlify function response"
-    };
+  const result = await octokit.rest.users.getAuthenticated()
+  
+  return {
+      statusCode: 200,
+      body: JSON.stringify(result)
+  }
 }
