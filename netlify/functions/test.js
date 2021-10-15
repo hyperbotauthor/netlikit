abefec2fc56d003e0da216f09950c05b369db1b4
hyperbotauthor <73212952+hyperbotauthor@users.noreply.github.com>
2021-10-15 07:05:25 +0000

    Test upload

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,12 +1,71 @@
 const { Octokit } = require("octokit")
+const fetch = require('node-fetch')
+
+const LICHESS_ACCOUNT_API_URL = "https://lichess.org/api/account"
 
 const octokit = new Octokit({ auth: process.env["GIT_TOKEN"] })
 
 exports.handler = async function(event, context) {
-  const result = await octokit.rest.users.getAuthenticated()
-  
-  return {
+  if(event.httpMethod === "POST"){
+    const request = JSON.parse(event.body)
+
+    const token = request.token
+
+    const response = await fetch(LICHESS_ACCOUNT_API_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    const accountBlob = await response.text()
+
+    const account = JSON.parse(accountBlob)
+
+    const id = account.id
+
+    const storePath = `lichess/${id}/${request.fileName}`
+
+    console.log("resolved account", id, storePath)
+
+    const buff = Buffer.from(request.content)
+
+    const base64data = buff.toString('base64')
+
+    const inforesp = await fetch(`https://api.github.com/repos/hyperbotauthor/blobs/contents/${storePath}`)
+
+    const info = JSON.parse(await inforesp.text())
+
+    console.log("blob size base64", base64data.length)
+
+    const create = await octokit.rest.repos.createOrUpdateFileContents({
+      owner: "hyperbotauthor",
+      repo: "blobs",
+      path: storePath,
+      message: "Store or update file by " + id,
+      content: base64data,
+      sha: info.sha || undefined
+    })
+
+    console.log(create)
+
+    const createSummary = {
+      storePath,
+      status: create.status,
+      contentSize: create.data.content.size
+    }
+
+    return {
       statusCode: 200,
-      body: JSON.stringify(result)
+      body: JSON.stringify(createSummary)
+    }
+  } else {
+    return {
+      statusCode: 200,
+      body: "you should use a POST request"
+    }
   }
+
+  //const result = await octokit.rest.users.getAuthenticated()
+  
+  
 }
