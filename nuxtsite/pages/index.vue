4c7c89b0b3d392895e0b7efeb3118e2cf35f928c
hyperbotauthor <73212952+hyperbotauthor@users.noreply.github.com>
2021-10-15 08:46:42 +0000

    Improve ui

--- nuxtsite/pages/index.vue
+++ nuxtsite/pages/index.vue
@@ -1,35 +1,39 @@
 <template>
-  <div>
+  <div style="width: 100%; text-align: center;">
+  <div style="display: inline-block; max-width: 600px; text-align: center; padding: 20px; border: solid 1px;">
     Upload content to <a href="https://github.com/hyperbotauthor/blobs/tree/main/lichess" target="_blank" rel="noopener noreferrer">https://github.com/hyperbotauthor/blobs/tree/main/lichess</a> under your lichess username resolved from your lichess access token ( this ensures that third parties cannot upload under your lichess username, so your content will be unique to you ).
 
     <hr>
 
     For your security lichess access token should have no scopes ticked, basic token just enough for resolving your lichess username.
 
-    <hr>
+    <br><br>
 
     <a href="https://lichess.org/account/oauth/token/create?description=netlikit" target="_blank" rel="noopener noreferrer">Create lichess access token</a>
 
     <hr>
 
-    lichess access token
+    Enter lichess access token
 
-    <hr>
+    <br><br>
 
     <input type="text" ref="token"></input>
 
     <hr>
 
-    Content
+    Enter content to upload
 
-    <hr>
+    <br><br>
 
     <textarea ref="content" style="width: 600px; height: 100px;"></textarea>
 
     <hr>
 
-    <button v-on:click="submit">Submit</button>
+    <br>
 
+    <button v-on:click="submit">Upload content</button>
+
+  </div>
   </div>
 </template>
 
@@ -69,6 +73,11 @@ export default {
   },
   mounted(){
     this.$refs.token.value = localStorage.getItem("LICHESS_TOKEN") || ""
+  },
+  head(){
+    return {
+      title: "Netlikit - upload to git by lichess"
+    }
   }
 }
 </script>
